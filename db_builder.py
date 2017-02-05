f1a86f2babb0ded872ead564ee1c924e164519b1
Richard Wang <rwang4@stuy.edu>
2017-02-04 20:06:20 -0500

    idk what i'm doing

--- db_builder.py
+++ db_builder.py
@@ -15,7 +15,16 @@ peeps = db.peeps
 dict = {}
 
 for key in d1:
-    doc = { ‘name’: key[0], ‘age’: key[1], 'id': key[2] }
-
-    
+    doc = { 'name': key['name'], 'age': key['age'], 'id': key['id'] }
     peeps.insert_one( doc )
+
+
+f2 = open('courses.csv')
+
+d2  = csv.DictReader(f2)
+
+courses = db.courses
+
+for key in d2:
+    doc = { 'code': key['code'], 'mark': key['mark'], 'id': key['id'] }
+    courses.inert_one( doc )
