1aed177b690362e9c4d2de70dc129409724b69f1
nicolecheng <ncheng@stuy.edu>
2017-02-05 19:41:42 -0500

    hm

--- db_builder.py
+++ db_builder.py
@@ -2,9 +2,10 @@ from pymongo import MongoClient
 import csv
 
 server = MongoClient()#'149.89.150.100')
-print server
+#print server
+
 db = server.mongeese 
-print db
+#print db
 
 # peeps - name, age, id
 
@@ -13,29 +14,20 @@ f1 = open('peeps.csv')
 d1 = csv.DictReader(f1)
 
 peeps = db.peeps
-print peeps
-dict = {}
 
-print "step 1"
+dict = {}
 
 for key in d1:
     doc = { 'name': key['name'], 'age': key['age'], 'id': key['id'] }
-    print doc
+    #print doc
     peeps.insert_one( doc )
 
-print "step 2"
-'''
 f2 = open('courses.csv')
 
 d2  = csv.DictReader(f2)
 
 courses = db.courses
 
-print "step 3"
-
 for key in d2:
     doc = { 'code': key['code'], 'mark': key['mark'], 'id': key['id'] }
-    courses.inert_one( doc )
-
-print "step 4"
-'''
+    courses.insert_one( doc )
